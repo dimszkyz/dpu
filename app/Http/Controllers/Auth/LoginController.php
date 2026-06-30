89bc869cc4bd12b11d5b51dfd5d5f8c1c4de6f1f
Dimas Rizky <schooldims@gmail.com>
2026-06-30 20:49:27 +0700

    Use clean login page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('auth.login-glass');
+        return view('auth.login-clean');
     }
 
     /**
