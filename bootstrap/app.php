9df786f52d3183f5d9895ba8f07f0847e3f991cf
Dimas Rizky <schooldims@gmail.com>
2026-07-02 23:35:00 +0700

    Use writable temp storage path on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,22 +3,25 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\RoleMiddleware; // 1. Tambahkan ini di atas
+use App\Http\Middleware\RoleMiddleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        
-        // 2. Tambahkan alias middleware di sini
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+if (getenv('VERCEL') || getenv('APP_STORAGE_PATH')) {
+    $app->useStoragePath(getenv('APP_STORAGE_PATH') ?: sys_get_temp_dir().'/laravel-storage');
+}
+
+return $app;
